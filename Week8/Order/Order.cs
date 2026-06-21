09365aded2e9ab311f23f7ff0c4bef2ccda753ba
Fatmanur GÜVEN <fatmanurguven82mf@gmail.com>
2026-06-21 14:17:11 +0300

    ilk calisma dosyalari eklendi

--- Week8/Order/Order.cs
+++
@@ -1,9 +0,0 @@
-namespace Order
-{
-    public class Order
-    {
-      public int Id { get; set; }
-      public string ProductName { get; set; }
-      public int Quantity { get; set; }
-    }
-}
